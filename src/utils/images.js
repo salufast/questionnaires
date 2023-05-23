b888ac7ebb5e8b51c277a74d8f31f531dc0810fb
agamags <ags.softprodigy@gmail.com>
2023-05-23 11:10:40 +0530

    adding OWL images

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,3 +2,5 @@
 export const Radio = require("../../assets/icons/Radio.png");
 export const Checkmark = require("../../assets/icons/Checkmark.png");
 export const Owl = require("../../assets/icons/Owl.png");
+export const LarknOwl = require('../../assets/icons/LarknOwl.png')
+export const LarkO = require('../../assets/icons/LarkO.png')
