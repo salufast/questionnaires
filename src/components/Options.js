a6060bc4ddab776fdd32c615ba7e8e1043d7820f
agamags <ags.softprodigy@gmail.com>
2023-06-07 14:11:00 +0530

    bug fixing + header change

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,11 +6,18 @@ import styles from './styles';
 import {Checkbox, Checkmark, Radio} from '../utils/images';
 import {buttonData} from '../utils/constant';
 import colors from '../utils/colors';
+import {useMutation} from '@apollo/client';
+import {INSERT_QUESTIONNAIRE_ANSWER} from '../apollo/questionnaire_answer';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 const Card = React.forwardRef(({item, multiple}, swiper) => {
   const scaleValue = useRef(new Animated.Value(1)).current;
   const [selectedOption, setSelectedOption] = useState(null);
   const [multiSelect, setMultiSelect] = useState([]);
+  const [response, setResponse] = useState(null);
 
   const handleSelectOption = option => {
     setSelectedOption(option);
@@ -18,10 +25,34 @@ const Card = React.forwardRef(({item, multiple}, swiper) => {
   const isCorrectOption = option => {
     return selectedOption === option.id;
   };
-
   useEffect(() => {
     if (selectedOption) handleButtonClick();
-  }, [selectedOption])
+  }, [selectedOption]);
+
+  const [insert_questionnaire_answer_one] = useMutation(
+    INSERT_QUESTIONNAIRE_ANSWER,
+    {
+      update(cache, {data: {insert_questionnaire_answer_one}}) {
+        cache.modify({
+          fields: {
+            answer(answerQuest = []) {
+              const newAnswer = cache.writeFragment({
+                data: insert_questionnaire_answer_one,
+                fragment: gql`
+                  fragment NewAnswer on Answer {
+                    id
+                    text
+                    index
+                  }
+                `,
+              });
+              return [...answerQuest, newAnswer];
+            },
+          },
+        });
+      },
+    },
+  );
 
   const handleButtonClick = () => {
     Animated.timing(scaleValue, {
@@ -45,13 +76,26 @@ const Card = React.forwardRef(({item, multiple}, swiper) => {
     }
   };
 
+  const caching = (questionnaireAnswerValues, response) => {
+    insert_questionnaire_answer_one({
+      variables: {questionnaireAnswerValues},
+      optimisticResponse: {
+        insert_questionnaire_answer_one: {
+          id: response?.id,
+          text: response?.text,
+          index: response?.index,
+        },
+      },
+    });
+  };
+
   return (
     <View style={styles.card}>
       <View>
-        <SaluText style={{color: colors.grey}}>
+        <SaluText style={styles.chooseAns}>
           {buttonData?.chooseAnswer}
         </SaluText>
-        <SaluTitle>{item?.title}</SaluTitle>
+        <SaluTitle style={{height: hp(12)}}>{item?.title}</SaluTitle>
         <View style={styles.questOpt}>
           {item?.answer_v2s.map((item, index) => {
             return (
@@ -70,6 +114,7 @@ const Card = React.forwardRef(({item, multiple}, swiper) => {
                   },
                 ]}
                 onPress={() => {
+                  setResponse(item);
                   multiple
                     ? handleOptionSelection(index)
                     : handleSelectOption(item.id);
@@ -104,18 +149,39 @@ const Card = React.forwardRef(({item, multiple}, swiper) => {
       </View>
       <View style={styles.mainButton}>
         <TouchableOpacity
-          style={styles.m_button}
+          style={styles.m_button_back}
           onPress={() => swiper.current.swipeBack()}>
-          <Text style={[styles.backText, {opacity: swiper?.current?.state?.firstCardIndex < 1 ? 0 : 1}]}>
+          <Text
+            style={[
+              styles.backText,
+              {opacity: swiper?.current?.state?.firstCardIndex < 1 ? 0 : 1},
+            ]}>
             {buttonData?.back}
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {
+            {
+              isCorrectOption(item);
+            }
             swiper?.current?.swipeLeft();
+            const questionnaireAnswerValues = {
+              question_answer_v2s: {
+                data: [
+                  {
+                    answer_text: response?.text,
+                    answer_v2_id: response?.id,
+                    question_v2_id: item?.id,
+                  },
+                ],
+              },
+              questionnaire_id: "",
+            };
+            // caching(questionnaireAnswerValues, response);
           }}
           style={[styles.m_button, {backgroundColor: colors.teal}]}>
-          <Text style={[styles.backText, {color: colors.white}]}>
+          <Text
+            style={styles.finishedText}>
             {swiper?.current?.state?.firstCardIndex === 4
               ? buttonData?.finished
               : buttonData?.next}
